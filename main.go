c6160d709b31644911312ffa12b80b5595d2dd4c
Koomball <135945730+Koomball@users.noreply.github.com>
2024-05-27 00:01:57 +1000

    Create main.go

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "image/png"
+    "log"
+    "strings"
+    "os"
+
+    gim "github.com/ozankasikci/go-image-merge"
+)
+
+type filePathStruct struct {
+    ImageFilePath string
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/tile/:w/:h/:id", makeImage)
+    
+    router.Run("localhost:8088")
+}
+
+func makeImage(c *gin.Context) {
+    id := c.Param("id")
+    var s = id
+    
+    height := c.Param("h")
+    var h = height
+    
+    height := c.Param("w")
+    var w = width
+  
+  
+    // create an array of the images we have
+    var images = strings.Split(s, "-")
+
+    // Create an array to store *gim.Grid instances
+    grids := make([]*gim.Grid, len(images))
+
+    // Iterate over the file paths and create *gim.Grid instances
+    for i, path := range images {
+        // we use a reference to the grid so we can modify the object outside the loop
+        grids[i] = &gim.Grid{ImageFilePath: "images/" + path + ".png"}
+    }
+    
+        // make the grid of images
+    rgba, err := gim.New(grids, w, h).Merge()
+    if err != nil {
+        log.Fatal(err)
+    }
+    // create the image
+    file, err := os.Create("./path.png")
+    err = png.Encode(file, rgba)
+}
